b9086f62c08eab479d6e4664c91447973e31b30b
JohnyDays <joao.l.d.a@hotmail.com>
2014-03-25 16:05:00 +0000

    Version 1.0, first working version

--- CanvasImageResizer.js
+++ CanvasImageResizer.js
@@ -4,37 +4,35 @@ CanvasImageResizer = new CanvasImageResizer()
 function CanvasImageResizer() {
     this.canvas = document.createElement('canvas');
     this.canResize = function canResize() {
-        return !!(canvas.getContext && canvas.getContext('2d'));
+        return !!(this.canvas.getContext && this.canvas.getContext('2d'));
     }
     this.resizeImageFromSource = function resizeImageFromSource(source, width, height, callback) {
-        var dataUrl;
-        var canvas = this.canvas;
-        canvas.width = width
-        canvas.height = height
-        context = canvas.getContext('2d')
+        var c = this.canvas
+        c.width = width
+        c.height = height
         imageObject = new Image();
         imageObject.src = source
         imageObject.onload = function() {
-            context.drawImage(imageObject, 0, 0, width, height)
-            callback(canvas.toDataURL(), source)
+            c.getContext('2d').clearRect(0, 0, c.width, c.height)
+            c.getContext('2d').drawImage(this, 0, 0, parseInt(width), parseInt(height))
+            callback(c.toDataURL(), source)
         }
 
     }
     this.resizeImageFromObject = function resizeImageFromObject(imageObject, width, height, callback) {
-        var dataUrl;
-        var canvas = this.canvas;
-        canvas.width = width
-        canvas.height = height
-        context = canvas.getContext('2d')
+        var c = this.canvas;
+        c.width = width
+        c.height = height
         if (imageObject.complete) {
-            context.drawImage(imageObject, 0, 0, width, height)
-            callback(canvas.toDataURL(), source)
+            c.getContext('2d').clearRect(0, 0, c.width, c.height)
+            c.getContext('2d').drawImage(imageObject, 0, 0, width, height)
+            callback(c.toDataURL(), imageObject)
         } else {
             imageObject.onload = function() {
-                context.drawImage(imageObject, 0, 0, width, height)
-                callback(canvas.toDataURL(), source)
+                c.getContext('2d').clearRect(0, 0, c.width, c.height)
+                c.getContext('2d').drawImage(imageObject, 0, 0, width, height)
+                callback(c.toDataURL(), imageObject)
             }
         }
-        callback(dataUrl, imageObject)
     }
 }
